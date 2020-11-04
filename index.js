ae5f1a97bcab1d323a3fb1eb7f5e2a35ebeabff5
Garth Bjerk <garth@drdropin.no>
2020-11-04 14:17:40 +0100

    Basic setup for a shareable eslint config

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: [],
+  rules: {},
+};
