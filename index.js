4cbf41925cbd40f2f50b40519c14db8a4f33cf53
Garth Bjerk <garth@drdropin.no>
2020-11-10 16:05:46 +0100

    Update rules based on web app xp

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 module.exports = {
   extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'prettier',
-    'prettier/react',
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "prettier",
+    "prettier/react",
   ],
   plugins: [],
-  rules: {},
+  rules: {
+    "no-use-before-define": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-param-reassign": ["error", { props: false }],
+  },
 };
